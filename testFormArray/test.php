cafd79cb54931a1d76557f39a7cb2901e1bb4df1
Nitigan Nakjuatong <nitigan.n@fitm.kmutnb.ac.th>
2021-03-05 13:39:20 +0700

    test Form Insert to Array

---
+++ testFormArray/test.php
@@ -0,0 +1,26 @@
+<form action='#' method='post'>
+<table border=1>
+
+<?php
+    for ($i=0; $i <10; $i++) { 
+        echo '<tr><td>';
+        echo '<input name=cars[] placeholder=Enter Car Name>';
+        echo '</td></tr>';
+    }
+    
+?>
+<tr><td>
+    <button name='submit'>Submit Information</button>
+</td></tr>
+</form>
+
+</table>
+
+<?php
+$cars = $_POST['cars'];
+foreach($cars as $showCar):
+    echo $showCar."<br>";
+endforeach;
+
+print_r($cars);
+?>
